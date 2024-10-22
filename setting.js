de04a76d518993b502afcba068be87a34da971cd
charuka-tech <168742032+charuka-tech@users.noreply.github.com>
2024-10-22 17:27:27 +0530

    Add files via upload

---
+++ setting.js
@@ -0,0 +1,17 @@
+
+const fs = require('fs');
+if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
+
+function convertToBool(text, fault = 'true') {
+    return text === fault ? true : false;
+}
+module.exports = {
+SESSION_ID: process.env.SESSION_ID||"eQ9TyYzS#B9x3lksI5m3r8tgJXYPw-RmZ1cca6AU5-s3yHAPa3ok",
+MONGODB: process.env.MONGODB || "mongodb+srv://sam:sam@cluster0.u1smxsv.mongodb.net",
+AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "false",
+//--------------------------------//--------------------------------//
+OMDB_API_KEY: process.env.OMDB_API_KEY="76cb7f39",
+OWNER_NAME: process.env.OWNER_NAME || "mr charuka",
+BOT_NAME: process.env.BOT_NAME || "mizuki md",
+FOOTER:process.env.FOOTER || "ᴍɪᴢᴜᴋɪ ᴍᴅ"
+};
