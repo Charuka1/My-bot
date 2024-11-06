4abc4372ad5fa09b8f5f0fe4c2548afbb947e1d1
charuka-tech <168742032+charuka-tech@users.noreply.github.com>
2024-11-06 20:28:22 +0530

    Update downloder.js

--- plugins/downloder.js
+++ plugins/downloder.js
@@ -279,8 +279,9 @@ let downloadUrl = down.dl_url
 
 //send audio + document message
 await conn.sendMessage(from,{audio: {url:downloadUrl},mimetype:"audio/mpeg"},{quoted:mek})
+await conn.sendMessage(from, { react: { text: '✅', key: audio.key }})
 await conn.sendMessage(from,{document: {url:downloadUrl},mimetype:"audio/mpeg",fileName:data.title + ".mp3",caption:"© ᴹᴬᴰᴱ ᴮʸ ᴰᴬᴿᴷ ᶜʸᴮᴱᴿ"},{quoted:mek})
-
+await conn.sendMessage(from, { react: { text: '✅', key: document.key }})
 }catch(e){
 console.log(e)
 reply(`${e}`)
@@ -339,9 +340,10 @@ let downloadUrl = down.dl_url
 //send video + document message
 
 await conn.sendMessage(from,{video: {url:downloadUrl},mimetype:"video/mp4"},{quoted:mek})
+await conn.sendMessage(from, { react: { text: '✅', key: video.key }})
 
 await conn.sendMessage(from,{document: {url:downloadUrl},mimetype:"video/mp4",fileName:data.title + ".mp4",caption:"© ᴹᴬᴰᴱ ᴮʸ ᴰᴬᴿᴷ ᶜʸᴮᴱᴿ"},{quoted:mek})
-
+await conn.sendMessage(from, { react: { text: '✅', key: document.key }})
 
 }catch(e){
 console.log(e)
