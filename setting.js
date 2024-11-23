08ccd852073dcbef5a87f3e0f92d0b5457ff6060
Charuka1 <129839125+Charuka1@users.noreply.github.com>
2024-11-23 12:13:14 +0530

    Update setting.js

--- setting.js
+++ setting.js
@@ -9,6 +9,7 @@ module.exports = {
 SESSION_ID: process.env.SESSION_ID||"T24A3SJI#BhengB7EwzUaZQaIwMhjatRmThFKjE9dA9_0pNxbIJU",
 MONGODB: process.env.MONGODB || "mongodb+srv://sam:sam@cluster0.u1smxsv.mongodb.net",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "false",
+MAX_SIZE: process.env.MAX_SIZE || "300",
 //--------------------------------//--------------------------------//
 OMDB_API_KEY: process.env.OMDB_API_KEY="76cb7f39",
 OWNER_NAME: process.env.OWNER_NAME || "mr charuka",
